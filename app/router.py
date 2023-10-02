33f5127e92c1aefaba0452baa51b47d0f5a9c1d8
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-10-02 17:16:38 +0100

    feat: reworked endpoints to match spec

---
+++ app/router.py
@@ -0,0 +1,90 @@
+import json
+import os
+from typing import Annotated, Any
+
+from fastapi import APIRouter, Depends, BackgroundTasks, Body, Form, UploadFile
+from fastapi.responses import FileResponse
+from pymongo.database import Database
+from pika.channel import Channel
+
+from app.db import get_db
+from app.services.video import create_video, merge_chunks, get_video
+from app.schemas.video import Chunk, VideoIn
+from app.services.backgroud_tasks import save_chunk
+from app.queque import get_channel
+from app.settings import settings
+
+
+router = APIRouter()
+
+
+@router.post('/upload/new', status_code=201)
+async def start_upload(
+    vid: VideoIn,
+    db: Database = Depends(get_db)
+):
+    """
+    starts a new upload
+    """
+    file_id, err = create_video(vid.file_type, db)
+
+    if err:
+        raise err
+
+    return {
+        'status_code': 201,
+        'message': 'Video upload started successfully',
+        'data': {
+            'file_id': file_id,
+        }
+    }
+
+
+@router.post('/upload/chunks', status_code=201)
+async def upload_chunks(
+    file_id: Annotated[str, Form()],
+    chunks: Annotated[UploadFile, Form(media_type='video/x-matroska')],
+    is_final: Annotated[bool, Form()],
+    chunk_num: Annotated[int, Form()],
+    background_tasks: BackgroundTasks,
+    channel: Channel = Depends(get_channel)
+):
+    """
+    uploads chunks of data
+    """
+    print(chunks)
+
+    os.makedirs(f'{settings.CHUNKS_DIR}/{file_id}', mode=0o771, exist_ok=True)
+
+    background_tasks.add_task(save_chunk, file_id, chunks, chunk_num)
+    if is_final:
+        print('final chunk recieved')
+        print(f'recieved {chunk_num} chunks')
+        merge_chunks(file_id, channel)
+
+    return {
+        'status_code': 201,
+        'message': 'Video chunks uploaded successfully',
+        'data': None
+    }
+
+
+@router.get('/videos/{video_id}')
+async def fetch_video(video_id: str, db: Database = Depends(get_db)):
+    """
+    fetches video given it's id
+    Returns a streaming response
+    """
+    file, err = get_video(video_id, db)
+
+    if err:
+        raise err
+
+    if not file.completed:
+        return {
+            'status_code': 200,
+            'message': 'Video is still being processed',
+            'data': None
+        }
+
+    return FileResponse(file['file_loc'], media_type='video/mp4')
--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,6 @@
-from decouple import config
+import os
+
+from decouple import config, AutoConfig
 
 
 class Settings:
@@ -7,7 +9,15 @@ class Settings:
     DB_NAME = config('DB_NAME', default='videos_store')
     DB_USER = config('DB_USER', default=None)
     DB_PASSWORD = config('DB_PASSWORD', default=None)
+    COLLECTION_NAME = config('COLLECTION_NAME', default='videos')
+    SAVE_DIR = config('SAVE_DIR', default='/tmp/videos')
+    CHUNKS_DIR = config('CHUNKS_DIR', default='/tmp/chunks')
     SERVER_URL = config('SERVER_URL', default='http://localhost:8001')
+    RABBITMQ_HOST = config('RABBITMQ_HOST', default='localhost')
+    RABBITMQ_PORT = config('RABBITMQ_PORT', default=5672, cast=int)
+    VIDEO_QUEUE = config('VIDEO_QUEUE', default='videos')
+    TRANSCRIBE_QUEUE = config('TRANSCRIBE_QUEUE', default='transcripts')
+    OPENAPI_SECRET = config('OPENAPI_SECRET')
 
 
 settings = Settings()
