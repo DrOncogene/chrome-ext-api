8a60386e6942e8fb23007d8aebc1d4672c7bd4a5
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-10-07 05:55:03 +0100

    feat: more robust merger consumer - reimplemented the combine_chunks helper function - add a retry and discard mechanism - always discard if chunks not found for whatever reason

--- merger.py
+++ merger.py
@@ -4,54 +4,101 @@ from sys import exit
 import shutil
 from time import sleep
 
-import moviepy.editor as mp
+import pika
+from pika.channel import Channel
+from pika import BasicProperties
 
 from app.queque import get_channel_unyield
 from app.db import get_db_unyield
 from app.settings import settings
-from app.helpers import combine_chunks_to_video
+from app.helpers import combine_chunks
 
 
-def merge(channel, method, properties, body):
+def merge(ch: Channel, method, properties: BasicProperties, body):
     """
-    consumer of VIDEO_QUEUE. merges chunks into a video
-    and publish to TRANSCRIBE_QUEUE
+    consumer of `VIDEO_QUEUE`. merges chunks into a video
+    and publish to a job to the `TRANSCRIBE_QUEUE`
     """
     sleep(5)
 
+    file_id: str = json.loads(body).get('file_id')
+    video_path = f'{settings.SAVE_DIR}/{file_id}.webm'
+    chunks_dir = f'{settings.CHUNKS_DIR}/{file_id}'
+
+    print(f'merging chunks for {video_path}...')
+
+    if properties.headers:
+        retries = properties.headers.get('x-retries', 0)
+    else:
+        properties.headers = {'x-retries': 0}
+        retries = 0
+
     try:
-        file_id: str = json.loads(body).get('file_id')
-        file_path = f'{settings.SAVE_DIR}/{file_id}'
-        chunks_dir = f'{settings.CHUNKS_DIR}/{file_id}'
 
         #  get a sorted list of paths to chunks
         chunks = sorted([chunk.path for chunk in os.scandir(chunks_dir)])
 
-        combine_chunks_to_video(chunks, f'{file_path}.mp4')  # combine chunks
+        success = combine_chunks(chunks, video_path)
 
-        # shutil.rmtree(f'{settings.CHUNKS_DIR}/{file_id}') #  remove chunks
+        if not success:
+            raise Exception('Unable to merge chunks')
+
+        shutil.rmtree(f'{settings.CHUNKS_DIR}/{file_id}')
 
         db, session = get_db_unyield()
         collection = db[settings.COLLECTION_NAME]
 
         result = collection.update_one(
             {'_id': file_id},
-            {'$set': {'completed': True, 'file_loc': f'{file_path}.mp4'}}
+            {'$set': {'completed': True, 'file_loc': video_path}}
         )
 
         session.end_session()
 
-        channel.basic_publish(
+        # publish transcription job
+        ch.basic_publish(
             exchange='',
             routing_key=settings.TRANSCRIBE_QUEUE,
             body=json.dumps({
                 'file_id': file_id,
-            })
+            }),
+            properties=pika.BasicProperties(
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+            )
         )
-        print(f'[*] Merging complete for video {file_id}')
-    except Exception as err:
-        print(f"[*] Unable to merge chunks for video {file_id}")
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        print(f'[*] Merging complete for video {video_path}')
+    except OSError as err:
         print(err)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        print(f'[*] CHUNKS NOT FOUND for video {video_path}')
+    except Exception as err:
+        print(err)
+
+        if retries < settings.MAX_RETRIES:
+            retries += 1
+
+            requeue_delay = float(2 ** retries)
+
+            properties.headers['x-retries'] = retries
+
+            sleep(requeue_delay)  # sleep for 2^retries seconds
+
+            # requeue job with modified properties/headers
+            ch.basic_publish(
+                exchange='',
+                routing_key=settings.VIDEO_QUEUE,
+                body=body,
+                properties=properties
+            )
+            ch.basic_reject(delivery_tag=method.delivery_tag, requeue=False)
+            print(f'[*] MERGING CHUNKS failed for video {video_path}, '
+                  f'requeueing for retry {retries}')
+        else:
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+            print(f"[*] UNABLE TO MERGE chunks for video {video_path} "
+                  f"after {retries} retries")
 
 
 def main():
@@ -59,11 +106,10 @@ def main():
     channel.queue_declare(queue=settings.VIDEO_QUEUE, durable=True)
     channel.basic_consume(
         queue='videos',
-        auto_ack=True,
         on_message_callback=merge
     )
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    print(' [*] Waiting for merging jobs. To exit press CTRL+C')
 
     channel.start_consuming()
 
@@ -72,7 +118,7 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print('MERGER SERVICE STOPPED ❌')
+        print('\nMERGER SERVICE STOPPED ❌')
         try:
             exit(0)
         except SystemExit:
